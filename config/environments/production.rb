8b17b46b82dad7e7264d922d6222772305f0342f
Muddaser Ahmed <muddaserahmed@gmail.com>
2013-09-14 21:55:02 +0500

    nothing new

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,8 @@ Recreal::Application.configure do
   # See everything in the log (default is :info)
   # config.log_level = :debug
 
+  config.log_level = ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].to_sym : ('info').to_sym
+
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
 
