b69d736693f8069e04cd4ac0794d253b2e6f9b78
Muddaser Ahmed <muddaserahmed@gmail.com>
2013-09-29 23:51:21 +0500

    nested form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+
+# Adding confirm message for association remove links.
+# Original : f.hidden_field(:_destroy) + link_to(name, '#', :class => "remove_fields #{is_dynamic ? 'dynamic' : 'existing'}")
+# Cocoon : ViewHelpers : link_to_remove_association
+# link_to_remove_association(name, f, args={})
+# name : to be displayed for link.
+# f : form object
+# args : defaults set to empty hash.
+#        this holds the html options like onClick.
+#        e.g : link_to_remove_association "name", f, { :onClick => "onclick();", onBlur => "onblur();" }
+# extra_class : If no html options are passed from the view it will append 
+#               the class to the class attribute of the link.
+#               This will call the original functionality
+# option_class : If class key added in view for styling purpose, this will add it to the args
+# is_dynamic : is as per the original functionality. Lets us know if the records is new or existing.
+def link_to_remove_association(name, f, args={})
+  extra_class = (args.has_key?(:onclick) || args.has_key?(:onClick)) ? nil : "remove_fields"
+  is_dynamic = f.object.new_record? ? 'dynamic' : 'existing'
+  option_class = args.has_key?(:class) ? args[:class] : nil
+
+  args.merge!({:class => "#{extra_class} #{is_dynamic} #{option_class}"})
+  f.hidden_field(:_destroy) + link_to(name, '#this', args)
+end
+
+
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,7 @@
 class Skill < ActiveRecord::Base
-  attr_accessible :name, :rating, :job_id
+  attr_accessible :name, :rating, :job_id, :_destroy
+  # NOT PERSISTENT
+attr_accessor :_destroy
+
   belongs_to :job
 end
